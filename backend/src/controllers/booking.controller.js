9ad1ab70cd768eea616acd2f82ec89f3de4b6878
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-11-17 16:06:13 +0300

    refactor getTable logic

--- backend/src/controllers/booking.controller.js
+++ backend/src/controllers/booking.controller.js
@@ -1,15 +1,14 @@
 import Table from "../models/Table.js";
 
-export const getTable = async (req, res) => {
+export const getTable = async (_, res) => {
   try {
-    const availableTables = await Table.find({ status: "active" });
-    const unavailableTables = await Table.find({ status: "disabled" });
+    const tables = await Table.find({ status: "active" });
 
-    if (!availableTables || !unavailableTables) {
-      return res.status(404).json({ message: "No tables found" });
+    if (tables.length === 0) {
+      return res.status(404).json({ message: "No available tables" });
     }
 
-    res.status(200).json(availableTables, unavailableTables);
+    res.status(200).json(tables);
   } catch (error) {
     console.error("Error getting tables", error.message);
     res.status(500).json({ message: "Internal server error" });
