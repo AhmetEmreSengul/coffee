b8cd4eeb13dc080fb298adf4185eb2c5ec54e881
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-11-26 02:29:24 +0300

    Refactor components for improved styling and layout consistency

--- frontend/src/components/AboutTimeSlot.tsx
+++ frontend/src/components/AboutTimeSlot.tsx
@@ -4,13 +4,13 @@ const AboutTimeSlot = () => {
       <img className=" md:h-155 md:w-[1700px] " src="./shop.png" alt="" />
       <div className="flex flex-col w-full h-155 bg-beige-50 ">
         <div className="my-auto">
-          <h1 className="text-3xl mt-10 md:text-6xl md:mt-0 flex gap-2 mb-10 ml-20 font-medium tracking-tight text-text-primary leading-tight font-serif text-center">
+          <h1 className="text-3xl  mt-10 md:text-6xl md:mt-0 inline-flex gap-2 mb-10 ml-20 font-medium tracking-tight text-text-primary leading-tight font-serif text-center">
             The Time Slot <br />
             <span className="text-transparent bg-clip-text bg-linear-to-r from-caramel-400 to-caramel-500 text-center italic">
               Café
             </span>
           </h1>
-          <p className=" p-5 md:pr-60 md:ml-20 text-lg font-light text-text-secondary">
+          <p className="text-lg p-5 xl:px-20 font-light text-text-secondary ">
             At The Time Slot, we believe great moments deserve their own space.
             That's why we've reimagined the café experience around something
             simple: time. Book your slot, step into a calm and cozy atmosphere,
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,15 +1,19 @@
 const Footer = () => {
   return (
-    <div className="bg-beige-100 w-full h-100 border-t border-border-light">
-      <div className="grid grid-cols-1 md:grid-cols-3 items-center">
+    <div className="bg-beige-100 w-full h-fit border-t border-border-light">
+      <div className="flex flex-col gap-5 md:gap-0 md:grid grid-cols-1 md:grid-cols-3 items-center">
         <div className="flex flex-col items-center ">
           <img className="size-72" src="./timeslot.png" alt="" />
           <p className="font-bold text-text-primary">Customer Support</p>
           <p className="font-light text-text-secondary">0 123 123 23</p>
-          <p className="font-bold text-md mt-5 text-caramel-500">support@timeslot.com</p>
+          <p className="font-bold text-md mt-5 text-caramel-500">
+            support@timeslot.com
+          </p>
         </div>
-        <div className="flex flex-col  gap-3">
-          <h1 className="font-bold text-lg mb-5 text-text-primary">Corporate</h1>
+        <div className="flex flex-col gap-3 w-80">
+          <h1 className="font-bold text-lg mb-5 text-text-primary">
+            Corporate
+          </h1>
           <p className="font-light text-text-secondary cursor-pointer hover:text-caramel-500 hover:underline transition">
             Corporate Membership Agreement
           </p>
@@ -29,8 +33,10 @@ const Footer = () => {
             Commercial Electronic Message Consent
           </p>
         </div>
-        <div className="flex flex-col gap-3">
-          <h1 className="font-bold text-lg mb-5 text-text-primary">Customer Services</h1>
+        <div className="flex flex-col gap-3 w-80">
+          <h1 className="font-bold text-lg mb-5 text-text-primary">
+            Customer Services
+          </h1>
           <p className="font-light text-text-secondary cursor-pointer hover:text-caramel-500 hover:underline transition">
             Customer Service
           </p>
@@ -40,8 +46,12 @@ const Footer = () => {
           <p className="font-light text-text-secondary cursor-pointer hover:text-caramel-500 hover:underline transition">
             Track Your Order
           </p>
-          <p className="font-light text-text-secondary cursor-pointer hover:text-caramel-500 hover:underline transition">Contact</p>
-          <p className="font-light text-text-secondary cursor-pointer hover:text-caramel-500 hover:underline transition">FAQ</p>
+          <p className="font-light text-text-secondary cursor-pointer hover:text-caramel-500 hover:underline transition">
+            Contact
+          </p>
+          <p className="font-light text-text-secondary cursor-pointer hover:text-caramel-500 hover:underline transition">
+            FAQ
+          </p>
           <p className="font-light text-text-secondary cursor-pointer hover:text-caramel-500 hover:underline transition">
             Mobile Applications
           </p>
