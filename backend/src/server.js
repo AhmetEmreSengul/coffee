52b45fd7de635f3e39d8545092cbbcbef2973c04
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-11-26 23:40:37 +0300

    Fix route handling for serving the frontend in production

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,7 @@ app.use("/book", bookingRoutes);
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
+  app.get("/*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
