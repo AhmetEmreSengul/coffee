b0c01189dd41628ac216df50002973cdcc623bea
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-11-26 23:44:25 +0300

    Fix static file serving route in production environment

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,8 +23,8 @@ app.use("/book", bookingRoutes);
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
