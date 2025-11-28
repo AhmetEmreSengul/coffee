c4a03812565922968ca388f6d21b4f26a256fa2d
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-11-28 03:35:12 +0300

    Update skeleton components to use caramel color scheme for improved visual consistency

--- frontend/src/components/CoffeDisplaySkeleton.tsx
+++ frontend/src/components/CoffeDisplaySkeleton.tsx
@@ -1,5 +1,5 @@
 const CoffeDisplaySkeleton = () => {
-  return <div className="skeleton w-70 h-100 bg-neutral-300 "></div>;
+  return <div className="skeleton w-70 h-100 bg-caramel-200 "></div>;
 };
 
 export default CoffeDisplaySkeleton;
--- frontend/src/components/TableCardSkeleton.tsx
+++ frontend/src/components/TableCardSkeleton.tsx
@@ -1,10 +1,11 @@
 const TableCardSkeleton = () => {
   return (
-    <div className="flex flex-col items-center my-auto mt-30">
-      <div className="skeleton size-85 md:size-120 bg-neutral-400" />
-      <div className="flex flex-col md:flex-row gap-5 mt-10">
-        <div className="skeleton size-65 bg-neutral-400"></div>
-        <div className="skeleton size-65 bg-neutral-400"></div>
+    <div className="flex flex-col items-center my-auto gap-5 mt-30 p-5">
+      <div className="skeleton size-85 md:size-120 bg-caramel-200" />
+      <div className="skeleton size-60 bg-caramel-200" />
+      <div className="flex gap-5 mt-10">
+        <div className="skeleton w-30 h-15 bg-caramel-200"></div>
+        <div className="skeleton w-30 h-15 bg-caramel-200"></div>
       </div>
     </div>
   );
