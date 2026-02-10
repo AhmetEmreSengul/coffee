6579a9bc5b090592a467f8279372d104e16c8f1e
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2026-02-10 23:30:51 +0300

    only display non-admin users for the dashboard

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -4,7 +4,11 @@ import User from "../models/User.js";
 
 export const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({}).select("-password");
+    const users = await User.find({
+      _id: { $ne: req.user._id },
+      role: "user",
+    }).select("-password");
+
     res.status(200).json(users);
   } catch (error) {
     console.error(error);
