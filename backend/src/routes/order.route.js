7872c1666cf56377e0f856a6e0a6ca43e54840b2
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2026-02-14 16:10:58 +0300

    Rate limiting to order and stripe routes

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -6,9 +6,11 @@ import {
 } from "../controllers/order.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { isBanned } from "../middleware/banned.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
 router.use(protectRoute);
 router.use(isBanned);
 
--- backend/src/routes/stripe.route.js
+++ backend/src/routes/stripe.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { createPayment } from "../controllers/stripe.controller.js";
 import { isBanned } from "../middleware/banned.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 router.use(isBanned);
 
 router.post("/create-payment-intent", createPayment);
