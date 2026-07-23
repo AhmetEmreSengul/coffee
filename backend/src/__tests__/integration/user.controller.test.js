abe7d69d65d3b690661ad044c5fc81a1356e9088
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2026-07-24 01:53:11 +0300

    Refactor user registration test: enhance assertions to verify JWT cookie presence and improve clarity in test description

--- backend/src/__tests__/integration/user.controller.test.js
+++ backend/src/__tests__/integration/user.controller.test.js
@@ -1,4 +1,11 @@
-import { afterAll, afterEach, beforeAll, describe, it } from "@jest/globals";
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  it,
+  expect,
+} from "@jest/globals";
 import mongoose from "mongoose";
 import supertest from "supertest";
 import app from "../../app";
@@ -8,6 +15,7 @@ import {
   connectTestDB,
 } from "../setup/dbHandler";
 import { createUserPayload } from "../fixtures/Users";
+import { response } from "express";
 
 describe("user", () => {
   beforeAll(connectTestDB);
@@ -16,8 +24,8 @@ describe("user", () => {
 
   describe("user registration route", () => {
     describe("given the input is valid", () => {
-      it("should return 201 and the user date", async () => {
-        const { statusCode, body } = await supertest(app)
+      it("should return 201 and the user date and set a jwt cookie", async () => {
+        const { statusCode, body, headers } = await supertest(app)
           .post("/auth/signup")
           .set("User-Agent", "jest")
           .send(createUserPayload);
@@ -28,6 +36,14 @@ describe("user", () => {
           fullName: expect.any(String),
           email: expect.any(String),
         });
+
+        expect(headers["set-cookie"]).toBeDefined();
+
+        const jwtCookie = headers["set-cookie"].find((cookie) =>
+          cookie.startsWith("jwt="),
+        );
+
+        expect(jwtCookie).toBeDefined();
       });
     });
   });
