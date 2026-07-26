bce3829e530c5e79dde18a5f694c95973403f05d
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2026-07-27 02:57:40 +0300

    Add environment check for test mode in arcjetProtection middleware

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -1,7 +1,12 @@
 import aj from "../lib/arcjet.js";
 import { isSpoofedBot } from "@arcjet/inspect";
+import { ENV } from "../lib/env.js";
 
 export const arcjetProtection = async (req, res, next) => {
+  if (ENV.ARCJET_ENV === "test") {
+    return next();
+  }
+
   try {
     const decision = await aj.protect(req);
 
