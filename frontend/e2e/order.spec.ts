55c3afcdab8ea44e99b14bb1e9727a44b4406583
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2026-08-04 23:08:34 +0300

    Add tests for order history functionality, including viewing, reordering, and deleting orders

--- frontend/e2e/order.spec.ts
+++ frontend/e2e/order.spec.ts
@@ -56,4 +56,114 @@ test.describe("order", () => {
 
     await expect(page.getByText("Order Created")).toBeVisible();
   });
+
+  test("user can login and check order history", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    await page.getByRole("link", { name: "Login" }).click();
+
+    await page
+      .getByPlaceholder("coffee@gmail.com")
+      .fill("fake.user@example.com");
+    await page.getByPlaceholder("Password").fill("hashed-test-password");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByRole("button", { name: "close" })).toBeVisible();
+
+    await page.getByRole("button", { name: "close" }).click();
+
+    await expect(page).toHaveURL("http://localhost:5173");
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+
+    await page.getByRole("link", { name: "Order History" }).click();
+
+    await expect(page.getByRole("radio").first()).toBeVisible();
+  });
+
+  test("user can login in and order again from order history", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:5173");
+
+    await page.getByRole("link", { name: "Login" }).click();
+
+    await page
+      .getByPlaceholder("coffee@gmail.com")
+      .fill("fake.user@example.com");
+    await page.getByPlaceholder("Password").fill("hashed-test-password");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByRole("button", { name: "close" })).toBeVisible();
+
+    await page.getByRole("button", { name: "close" }).click();
+
+    await expect(page).toHaveURL("http://localhost:5173");
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+
+    await page.getByRole("link", { name: "Order History" }).click();
+    await page.getByRole("radio").first().click();
+    await page.getByRole("button", { name: "Order Again" }).click();
+
+    await page.getByRole("link", { name: "1" }).click();
+
+    await page.getByRole("textbox", { name: "Order Note" }).fill("test note");
+    await page
+      .getByTestId("card-number")
+      .locator("iframe")
+      .contentFrame()
+      .getByRole("textbox")
+      .fill("4242424242424242");
+
+    await page
+      .getByTestId("card-expiry")
+      .locator("iframe")
+      .contentFrame()
+      .getByRole("textbox")
+      .fill("1230");
+
+    await page
+      .getByTestId("card-cvc")
+      .locator("iframe")
+      .contentFrame()
+      .getByRole("textbox")
+      .fill("123");
+
+    await expect(page.getByRole("button", { name: "close" })).toBeVisible();
+    await page.getByRole("button", { name: "close" }).click();
+
+    await expect(page.getByRole("button", { name: "Pay 140₺" })).toBeEnabled();
+
+    await page.getByRole("button", { name: "Pay 140₺" }).click();
+
+    await expect(page.getByText("Order Created")).toBeVisible();
+  });
+
+  test("user can login and delete an order", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    await page.getByRole("link", { name: "Login" }).click();
+
+    await page
+      .getByPlaceholder("coffee@gmail.com")
+      .fill("fake.user@example.com");
+    await page.getByPlaceholder("Password").fill("hashed-test-password");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByRole("button", { name: "close" })).toBeVisible();
+
+    await page.getByRole("button", { name: "close" }).click();
+
+    await expect(page).toHaveURL("http://localhost:5173");
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+
+    await page.getByRole("link", { name: "Order History" }).click();
+    await page.getByRole("radio").first().click();
+    await page.getByRole("button", { name: "Delete" }).click();
+    await page.getByRole("button", { name: "Yes I'm sure." }).click();
+
+    await expect(page.getByText("Order deleted.")).toBeVisible();
+  });
 });
